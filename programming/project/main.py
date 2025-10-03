eababe331b9fdacd6450eb646250dd216b9e4876
Santiago Suarez <santiago.suarezpungitore@gmail.com>
2025-10-03 11:48:27 +0200

    Add formatting config fully (only works with window's version of python)

--- programming/project/main.py
+++ programming/project/main.py
@@ -1,12 +1,14 @@
 from mininumpy.array import MiniNumPy as mnp
 
-def main() -> None:
-    print("This is a test")
-    array = mnp.array([1,2,3,4])
-    print(array.stored_data)
 
-    array_new = mnp.zeros((2,3))
-    print(array_new)
+def main() -> None:
+	print("This is a test")
+	array = mnp.array([1, 2, 3, 4])
+	print(array.stored_data)
+
+	array_new = mnp.zeros((2, 3))
+	print(array_new)
+
 
 if __name__ == "__main__":
-    main()
+	main()
