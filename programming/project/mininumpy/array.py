f247d4579b6ffd1f51cc33c485a18666ea72e4f4
Santiago Suarez <santiago.suarezpungitore@gmail.com>
2025-10-10 18:34:38 +0200

    Updated edge cases and tidy things up

--- programming/project/mininumpy/array.py
+++ programming/project/mininumpy/array.py
@@ -1,5 +1,8 @@
+# File with
+
+
 class Array:
-	"""Array data to emulate numpy class"""
+	"""Array to implement lite version of Numpy."""
 
 	data: memoryview
 	data_list = list
@@ -7,25 +10,6 @@ class Array:
 	dtype: type[int] | type[float] | type[None]
 	ndim: int
 	size: int
-
-	# TODO: get the memoryview thing correctly
-	# TODO: check the type consitency, if casting is necessary
-	def __init__(self, input_list: list[int | float]):
-		"""
-		Creates Array object from given list,
-
-		This class only accepts same type (int or float) multi-nested arrays of homegenous
-		dimension. If the input list does not match these conditions, and exception will be raised
-		at runtime.
-		"""
-
-		self.shape, self.dtype = self._get_list_shape(input_list)
-		self.ndim = len(self.shape)
-		self.size = self._multiply_int_list(self.shape)
-
-		# actually store data
-		self.data_list = self._flatten_list(input_list)
-		# TODO: data in memoryview still missing
 
 	@staticmethod
 	def _multiply_int_list(lst: list[int]) -> int:
@@ -42,20 +26,25 @@ class Array:
 	@classmethod
 	def _get_list_shape(
 		cls,
-		test_list: int | float | list,
+		test_list: int | float | list[int | float | None],
 	) -> tuple[tuple[int], type[int] | type[float] | type[None]]:
 		"""
-		Check if the list has consistent dimensions and types.
-		In numpy jargon, checks the homogeneity of the input list.
+		Get the shape and type of a multi-nested list.
+		If the dimensions do not form an 'n-dimensional square' (if they are not homogeneous in
+		numpy jargon), or if the types do not match, an appropriate exeption will be raised.
+
+		Value error will be raised if non-expected object is inputted to the function.
 		"""
 
-		# if the current object is a list, checks recursively for the
-		# shape of all sublists. If these are consistent, then returns
-		# its shape, prepended to the shape of the sublists.
+		# If the current object is a list, checks recursively for the
+		# shapes and types of all sublists.
+		#
+		# If these shapes and types are consistent, then the main list's shape is its length
+		# preppended to the shape of any of its sublists (which should be the same).
 		if isinstance(test_list, list):
 			length = len(test_list)
 			if length == 0:
-				return (0, None)
+				return ((0,), None)
 
 			shape_dtype_list = [cls._get_list_shape(elem) for elem in test_list]
 			first_shape = shape_dtype_list[0][0]
@@ -63,15 +52,14 @@ class Array:
 			for shape, dtype in shape_dtype_list[1:]:
 				if shape != first_shape:
 					raise ValueError("Inconsistent shape between sublists")
-				if not (first_dtype == dtype):
+				if first_dtype != dtype:
 					raise ValueError("Inconsistent typing between sublists' elements")
 			return (length, *first_shape), first_dtype
 
 		if cls._is_int_or_float(test_list):
 			return (), type(test_list)
 
-		else:
-			return ValueError("Cannot get the shape of non-list, non-nummber type")
+		raise ValueError("Cannot get the shape of non-list, non-number type")
 
 	@classmethod
 	def _flatten_list(cls, lst: list | int | float) -> list:
@@ -87,8 +75,23 @@ class Array:
 
 		return flattened_list
 
-	# From the memoryview, with a sanity check on the current shape
-	# and checking numpy's behaviour, it should be easy
+	# TODO: get the memoryview thing correctly if necessary.
+	def __init__(self, input_list: list[int | float]):
+		"""
+		Creates Array object from given list,
+
+		This class only accepts same type (int or float) multi-nested arrays of homegenous
+		dimension. If the input list does not match these conditions, and exception will be raised
+		at runtime.
+		"""
+
+		self.shape, self.dtype = self._get_list_shape(input_list)
+		self.ndim = len(self.shape)  # in case of empty list input, ndim = 1 (same as numpy)
+		self.size = self._multiply_int_list(self.shape)
+
+		# actually store data
+		self.data_list = self._flatten_list(input_list)
+
 	def reshape(self, new_shape: tuple[int]):
 		if self._multiply_int_list(new_shape) != self.size:
 			raise RuntimeError("size of input shape does not correspond to size of current array")
@@ -96,7 +99,7 @@ class Array:
 
 	# From its current shape, and checking that the permutation of
 	# dimensions is correct, again, checking numpy's behaviour, it
-	# should be easy
+	# should be easy.
 	def transpose(self, permutation: tuple[int] | None = None):
 		if permutation is None:
 			pass
@@ -109,14 +112,20 @@ class Array:
 		flattened_list: list[int | float | None],
 		shape: tuple[int],
 	) -> list:
-		# shape must be valid, otherwise unexpected results will be obtained.
+		"""
+		Unflattens given list to specified shape.
+		"""
+		# sanity assertion
+		if cls._multiply_int_list(shape) != len(flattened_list):
+			raise ValueError("Given list size does not match size obtained from given shape.")
 
+		# empty list and dtype list cases
 		if flattened_list == []:
 			return flattened_list
 		if (len(shape) == 0) or (len(shape) == 1):
 			return flattened_list
 
-		print("Recursive call")
+		# recursive call
 		unflattend_list = []
 		sublength = cls._multiply_int_list(shape[1:])
 		for idx in range(shape[0]):
@@ -128,7 +137,7 @@ class Array:
 
 		return unflattend_list
 
-	# check exactly how the pretty printing works here.
+	# for pretty printing purposes
 	def __str__(self):
 		return self._unflatten_list(self.data_list, self.shape).__str__()
 
