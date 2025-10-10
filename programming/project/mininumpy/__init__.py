f20b990e0b5401454ec051e032d425ccf3eb2ccb
Santiago Suarez <santiago.suarezpungitore@gmail.com>
2025-10-10 18:52:21 +0200

    Sort some package things

---
+++ programming/project/mininumpy/__init__.py
@@ -0,0 +1,3 @@
+from .core import MiniNumPy
+
+__version__ = "0.1.0"
--- programming/project/mininumpy/array.py
+++ programming/project/mininumpy/array.py
@@ -102,9 +102,10 @@ class Array:
 	# should be easy.
 	def transpose(self, permutation: tuple[int] | None = None):
 		if permutation is None:
+			# turn the indices around
 			pass
-		if set(permutation) is not set(range(len(permutation))):
-			raise RuntimeError("Permutation invalid.")
+		if set(permutation) != set(range(len(permutation))):
+			raise RuntimeError("Invalid permutation for transposition.")
 
 	@classmethod
 	def _unflatten_list(
@@ -142,97 +143,3 @@ class Array:
 		return self._unflatten_list(self.data_list, self.shape).__str__()
 
 	__repr__ = __str__
-
-
-class MiniNumPy:
-	"""
-	Namespace for all MiniNumPy constructor methods
-	"""
-
-	@staticmethod
-	def array(list_or_nested_list: list) -> Array:
-		"""Creates array from a given list"""
-		# Sanitize array
-		return Array(list_or_nested_list)
-
-	@classmethod
-	def _singular_value_array(cls, shape: tuple[int], value: int | float) -> Array:
-		"""Builds an array of the specified shape, with given value."""
-		size = 1
-		for dim_length in shape:
-			size *= dim_length
-
-		current_value = value
-		for dim in shape[::-1]:
-			if isinstance(current_value, int) or isinstance(current_value, float):
-				current_list_level = [current_value for _ in range(dim)]
-			else:  # should be list instance. Copies it along new axis.
-				current_list_level = [current_value.copy() for _ in range(dim)]
-			current_value = current_list_level
-
-		return cls.array(current_list_level)
-
-	@classmethod
-	def zeros(cls, shape: tuple[int]) -> Array:
-		"""Returns an array of zeros of the specified shape."""
-		return cls._singular_value_array(shape, 0)
-
-	@classmethod
-	def ones(cls, shape: tuple[int]) -> Array:
-		"""Returns an array of ones of the specified shape."""
-		return cls._singular_value_array(shape, 1)
-
-	@classmethod
-	def eye(cls, n: int) -> Array:
-		"""Returns a square array of shape (n,n) with ones in its diagonal."""
-		# creates 2-dimensional list with proper shape and values
-		lst = [
-			[1 if idx_2 == idx else 0 for idx_2, __ in enumerate(range(n))]
-			for idx, _ in enumerate(range(n))
-		]
-		# converts to array and returns
-		return cls.array(lst)
-
-	@staticmethod
-	def _check_range(start: float, stop: float) -> None:
-		"""Helper method to check validity of range and raise exception otherwise"""
-		if start > stop:
-			raise RuntimeError("Invalid range, start > stop")
-
-	@staticmethod
-	def _check_postive(number: float, name: str) -> None:
-		"""Helper method to check validity of positive number and raise exception otherwise"""
-		if number <= 0:
-			raise RuntimeError(f"Invalid value ( <=0 ) for {name}")
-
-	@classmethod
-	def arange(cls, start: float, stop: float, step: float) -> Array:
-		# Array of values from [start,stop), with difference of
-		# step in between each pair
-
-		# sanitize input
-		cls._check_range(start, stop)
-		cls._check_positive(step, "step")
-
-		base_list = []
-		idx = 0
-		while start + idx * step < stop:
-			base_list.append(start + step * idx)
-			idx += 1
-
-		return cls.array(base_list)
-
-	@classmethod
-	def linspace(cls, start: float, stop: float, num: int) -> Array:
-		# evenly num-spaced values in the interval [start,stop)
-
-		# sanitize input
-		cls._check_range(start, stop)
-		cls._check_positive(num, "num")
-
-		diff = (stop - start) / num
-		base_list = []
-		for idx in range(num):
-			base_list.append(start + diff * idx)
-
-		return cls.array(base_list)
