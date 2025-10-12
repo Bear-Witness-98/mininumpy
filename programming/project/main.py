07d8c557dc5c75b38fa0941a3b6c34bddc42bbd7
Santiago Suarez <santiago.suarezpungitore@gmail.com>
2025-10-12 17:23:10 +0200

    Fix printing of array

--- programming/project/main.py
+++ programming/project/main.py
@@ -1,10 +1,10 @@
-from mininumpy.array import MiniNumPy as mnp
+import mininumpy as mnp
 
 
 def main() -> None:
 	print("This is a test")
 	array = mnp.array([1, 2, 3, 4])
-	print(array.stored_data)
+	print(array)
 
 	array_new = mnp.zeros((2, 3))
 	print(array_new)
