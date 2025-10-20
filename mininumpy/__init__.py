d0788d01b9a4fbdb2e1c7233c2fbb32bbd92e904
Santiago Suarez <santiago.suarezpungitore@gmail.com>
2025-10-20 15:03:50 +0200

    Tidy up

--- mininumpy/__init__.py
+++ mininumpy/__init__.py
@@ -1,3 +1,3 @@
-from .core import abs, arange, array, exp, eye, linspace, log, ones, sqrt, zeros
+from .core import Array, abs, arange, array, exp, eye, linspace, log, ones, sqrt, zeros
 
 __version__ = "0.1.0"
